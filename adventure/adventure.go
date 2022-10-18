6f0ad7659e0793a3c8aea1eb677795e8fbc4f1d0
majewski-piotr <majewski.piotr.511@gmail.com>
2022-10-18 23:45:24 +0200

    Added cli game mode and text template

--- adventure/adventure.go
+++ adventure/adventure.go
@@ -1,8 +1,10 @@
 package adventure
 
 import (
-	"html/template"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 type Adventure struct {
@@ -17,8 +19,13 @@ type Option struct {
 }
 
 type AdventureHandler struct {
-	AM map[string]Adventure
-	T  *template.Template
+	AM       map[string]Adventure
+	Template Executable
+}
+
+// interface to match both http and text template
+type Executable interface {
+	Execute(wr io.Writer, data any) error
 }
 
 // Handles requests from html pages generated from
@@ -26,5 +33,27 @@ type AdventureHandler struct {
 // mach keys in a given map
 func (ah AdventureHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path[1:]
-	ah.T.Execute(rw, ah.AM[path])
+	ah.Template.Execute(rw, ah.AM[path])
+}
+
+// Runs adventures through cli, outputs through os.Strout,
+// reads input from the console via fmt.Scanln
+func (ah AdventureHandler) RunCli() {
+	scene := "intro"
+	for {
+		a := ah.AM[scene]
+		ah.Template.Execute(os.Stdout, a)
+
+		if len(a.Options) == 0 {
+			os.Exit(0)
+		}
+
+		var optionNumber int
+		fmt.Scanln(&optionNumber)
+		if optionNumber >= 0 && optionNumber < len(a.Options) {
+			scene = a.Options[optionNumber].Arc
+		} else {
+			fmt.Printf("Option %v is invalid, repeating scene\n", optionNumber)
+		}
+	}
 }
--- adventure/main/main.go
+++ adventure/main/main.go
@@ -5,20 +5,19 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"html/template"
+	ht "html/template"
+	"log"
 	"net/http"
 	"os"
+	tt "text/template"
 )
 
 func main() {
+	typePtr := flag.String("type", "web", "type of application, cli or web")
 	jsonPtr := flag.String("json", "gopher.json", "path to sory file in json format")
-	templatePtr := flag.String("template", "adventure.gohtml", "path to html template file")
+	templatePtr := flag.String("template", "adventure.gohtml", "path to template file. use html for web and text for cli")
 	flag.Parse()
 
-	tpl, tplErr := template.ParseFiles(*templatePtr)
-	if tplErr != nil {
-		panic(tplErr)
-	}
 	jsonBytes, jsonErr := os.ReadFile(*jsonPtr)
 	if jsonErr != nil {
 		panic(jsonErr)
@@ -26,12 +25,41 @@ func main() {
 	var am map[string]adventure.Adventure
 	json.Unmarshal(jsonBytes, &am)
 
+	switch *typePtr {
+	case "web":
+		runWeb(templatePtr, am)
+	case "cli":
+		runCli(templatePtr, am)
+	default:
+		log.Fatal("Wrong type of app:", *typePtr)
+	}
+
+}
+func runCli(templatePtr *string, am map[string]adventure.Adventure) {
+	tpl, tplErr := tt.ParseFiles(*templatePtr)
+	if tplErr != nil {
+		panic(tplErr)
+	}
+
 	ah := adventure.AdventureHandler{
-		AM: am,
-		T:  tpl,
+		AM:       am,
+		Template: tpl,
+	}
+
+	ah.RunCli()
+}
+
+func runWeb(templatePtr *string, am map[string]adventure.Adventure) {
+	tpl, tplErr := ht.ParseFiles(*templatePtr)
+	if tplErr != nil {
+		panic(tplErr)
+	}
+
+	ah := adventure.AdventureHandler{
+		AM:       am,
+		Template: tpl,
 	}
 
 	fmt.Println("Starting the server on :8082")
 	http.ListenAndServe(":8082", ah)
-
 }
