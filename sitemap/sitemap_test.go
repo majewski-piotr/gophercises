7f6c3bade96351b5dfb1b8fb47d06589ef6faaec
majewski-piotr <majewski.piotr.511@gmail.com>
2022-10-27 19:24:06 +0200

    updated test

--- sitemap/sitemap_test.go
+++ sitemap/sitemap_test.go
@@ -1,6 +1,12 @@
 package sitemap
 
-import "testing"
+import (
+	"bytes"
+	"log"
+	"os"
+	"strings"
+	"testing"
+)
 
 func TestContains(t *testing.T) {
 	d := Domain{Name: "elderscrolls.org", Sites: nil}
@@ -19,5 +25,26 @@ func TestContains(t *testing.T) {
 
 	if d.contains("http://www.elderscrolls.oblivion/badfaces") {
 		t.Error("http://www.elderscrolls.oblivion/badfaces isn't from domain elderscrolls")
+	}
+}
+
+func TestContainsError(t *testing.T) {
+	//given
+	d := Domain{Name: "elderscrolls.org", Sites: nil}
+
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+	defer func() {
+		log.SetOutput(os.Stderr)
+	}()
+
+	//when
+	d.contains("HTTascxsvcvsdd?#$%!aax")
+	actual := buf.String()[20:]
+
+	//then
+	expected := `during parsing url of HTTascxsvcvsdd?#$%!aax occured error:parse "HTTascxsvcvsdd?#$%!aax": invalid URL escape "%!a"`
+	if strings.EqualFold(expected, actual) {
+		t.Errorf("Wrong err log, should be \n%v\ngot\n%v", expected, actual)
 	}
 }
